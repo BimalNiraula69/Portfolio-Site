49d43aa23e28cdf19b8e2b00229d567b4aed66cb
BimalNiraula69 <bimalniraula69@gmail.com>
2025-02-13 23:10:18 +0545

    fixed

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -29,24 +29,23 @@ const Portfolio = () => {
           </span>folio
         </h2>
         <div className="flex justify-center flex-col content-center relative w-full max-w-5xl h-130 overflow-hidden rounded-lg shadow-lg bg-white mx-auto px-4 sm:px-6 lg:px-8">
-          {/* Slides */}
-          {images.map((image, index) => (
-            <motion.div
-              key={index}
-              initial={{ opacity: 0, x: index === current ? 0 : 100 }}
-              animate={{ opacity: index === current ? 1 : 0, x: 0 }}
-              transition={{ duration: 0.5 }}
-              className={`absolute inset-0 w-full h-full ${index === current ? "block" : "hidden"}`}
-            >
-              <img src={image.src} alt={image.title} className="w-full h-full object-cover rounded-lg" />
-              <div className="flex flex-col justify-center align-center absolute bottom-5 left-5 sm:left-10 lg:left-20">
-                <h5 className="text-lg font-bold">{image.title}</h5>
-                <p className="text-sm">{image.text}</p>
-              </div>
-            </motion.div>
-          ))}
+                {images.map((image, index) => (
+                <motion.div
+                  key={index}
+                  initial={{ opacity: 0, x: index === current ? 0 : 100 }}
+                  animate={{ opacity: index === current ? 1 : 0, x: 0 }}
+                  transition={{ duration: 0.5 }}
+                  className={`absolute inset-0 w-full h-full ${index === current ? "block" : "hidden"}`}
+                >
+                  <img src={image.src} alt={image.title} className="w-full h-full object-cover rounded-lg mb-16" />
+                  <div className="flex flex-col justify-center align-center absolute bottom-16 left-5 sm:left-10 lg:left-20">
+                  <h5 className="text-lg font-bold">{image.title}</h5>
+                  <p className="text-sm">{image.text}</p>
+                  </div>
+                </motion.div>
+                ))}
 
-          {/* Indicators */}
+                {/* Indicators */}
           <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 flex gap-3">
             {images.map((_, index) => (
               <button
