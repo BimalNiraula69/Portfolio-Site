9e555359b15bc4047686c3ed29254043b1897a7a
BimalNiraula69 <bimalniraula69@gmail.com>
2025-02-16 16:36:37 +0545

    favicon added

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,6 +1,5 @@
 import bimal from './bimal.jpg';
 import contact_us from './contact_us.png';
-import home from './home.png';
 import information_button from './information_button.png';
 import portfolio from './portfolio.png';
 import post from './post.png';
@@ -10,7 +9,6 @@ import animated_video from './animated_video.mp4';
 const assets ={
     bimal,
     contact_us,
-    home,
     information_button,
     portfolio,
     post,
